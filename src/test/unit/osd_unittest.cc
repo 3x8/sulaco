9131ea68f07f5c987df7c17a8126990f0f34e317
adrianmiriuta <adrian.miriuta@gmail.com>
2018-11-22 11:06:23 +0100

    imuf_mode_48 accAverage

--- src/test/unit/osd_unittest.cc
+++ src/test/unit/osd_unittest.cc
@@ -71,7 +71,7 @@ extern "C" {
     gpsSolutionData_t gpsSol;
 
     acc_t acc;
-    float accAverage[XYZ_AXIS_COUNT];
+    //float accAverage[XYZ_AXIS_COUNT];
 
     PG_REGISTER(batteryConfig_t, batteryConfig, PG_BATTERY_CONFIG, 0);
     PG_REGISTER(blackboxConfig_t, blackboxConfig, PG_BLACKBOX_CONFIG, 0);
