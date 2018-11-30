f27f5222e66ff568603881e7acde52bd87289249
adrianmiriuta <adrian.miriuta@gmail.com>
2018-11-30 10:40:37 +0100

    cleanup

--- src/main/flight/imu.h
+++ src/main/flight/imu.h
@@ -1,23 +1,3 @@
-/*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include "common/axis.h"
@@ -27,18 +7,18 @@
 
 #ifndef DEFAULT_ATTITUDE_UPDATE_INTERVAL
 #define DEFAULT_ATTITUDE_UPDATE_INTERVAL 200
-#endif //DEFAULT_ATTITUDE_UPDATE_INTERVAL 200
+#endif
 
-// Exported symbols
 extern uint32_t accTimeSum;
 extern int accSumCount;
 extern float accVelScale;
 extern int32_t accSum[XYZ_AXIS_COUNT];
 
+// absolute inclination in multiple of 0.1 degree    180 deg = 1800
 typedef union {
     int16_t raw[XYZ_AXIS_COUNT];
     struct {
-        // absolute angle inclination in multiple of 0.1 degree    180 deg = 1800
+
         int16_t roll;
         int16_t pitch;
         int16_t yaw;
@@ -51,13 +31,13 @@ extern quaternion qHeadfree;
 extern quaternion qAttitude;
 
 typedef struct accDeadband_s {
-    uint8_t xy;                 // set the acc deadband for xy-Axis
-    uint8_t z;                  // set the acc deadband for z-Axis, this ignores small accelerations
+    uint8_t xy;
+    uint8_t z;
 } accDeadband_t;
 
 typedef struct imuConfig_s {
-    uint16_t dcm_kp;                        // DCM filter proportional gain ( x 10000)
-    uint16_t dcm_ki;                        // DCM filter integral gain ( x 10000)
+    uint16_t dcm_kp;                        // DCM proportional gain ( x 10000)
+    uint16_t dcm_ki;                        // DCM integral gain ( x 10000)
     uint8_t small_angle;
     uint8_t acc_unarmedcal;                 // turn automatic acc compensation on/off
     accDeadband_t accDeadband;
