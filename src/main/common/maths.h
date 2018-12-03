8fddcc3b2ee23f378c3d13172ea5b3a6bbe77737
adrianmiriuta <adrian.miriuta@gmail.com>
2018-12-03 12:23:55 +0100

    variable cleanup

--- src/main/common/maths.h
+++ src/main/common/maths.h
@@ -28,8 +28,8 @@
 #define power3(x) ((x)*(x)*(x))
 
 // Undefine this for use libc sinf/cosf. Keep this defined to use fast sin/cos approximations
-#define FAST_MATH             // order 9 approximation
-#define VERY_FAST_MATH      // order 7 approximation
+//#define FAST_MATH             // order 9 approximation
+//#define VERY_FAST_MATH        // order 7 approximation
 
 // Use floating point M_PI instead explicitly.
 #define M_PIf       3.14159265358979323846f
