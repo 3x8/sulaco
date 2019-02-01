d14bbdda51bab039a5ff8de151e9faef12927beb
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-02-01 15:02:14 +0100

    tfp_printf header

--- src/main/common/printf.h
+++ src/main/common/printf.h
@@ -109,7 +109,7 @@ regs Kusti, 23.10.2004
 void init_printf(void *putp, void (*putf) (void *, char));
 
 // Disabling this, in favour of tfp_format to be used in cli.c
-//int tfp_printf(const char *fmt, ...);
+int tfp_printf(const char *fmt, ...);
 int tfp_sprintf(char *s, const char *fmt, ...);
 
 int tfp_format(void *putp, void (*putf) (void *, char), const char *fmt, va_list va);
