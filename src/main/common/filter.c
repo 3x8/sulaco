a7fb8106a20782525f8d060318a2f8d7888f946d
adrianmiriuta <adrian.miriuta@gmail.com>
2019-03-04 13:01:10 +0100

    cleanup

--- src/main/common/filter.c
+++ src/main/common/filter.c
@@ -210,7 +210,7 @@ FAST_CODE float kalmanUpdate(kalman_t *filter, float input) {
     filter->p = (1.0f - filter->k) * filter->p;
 
     //DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_Q, lrintf(filter->q * 1000));
-    DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_Q, lrintf(ABS(input - filter->x)  * 1000));
+    DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_Q, lrintf(ABS(input - filter->x)  * 10));
     DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_R, lrintf(filter->r));
     DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_P, lrintf(filter->p));
     DEBUG_SET(DEBUG_KALMAN, DEBUG_KALMAN_K, lrintf(filter->k * 1000));
