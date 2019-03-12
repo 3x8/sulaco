8f188fc3dab0723ec1c0b72fe1172a2bfdb71433
adrianmiriuta <adrian.miriuta@gmail.com>
2019-03-12 08:01:33 +0100

    test serial

--- src/main/target/CLRACINGF7/target.h
+++ src/main/target/CLRACINGF7/target.h
@@ -54,6 +54,7 @@
 #define UART1_RX_PIN            PA10
 #define UART1_TX_PIN            PA9
 
+/*
 #define USE_UART2
 #define UART2_RX_PIN            PA3
 #define UART2_TX_PIN            PA2
@@ -64,15 +65,16 @@
 
 #define USE_UART4
 #define UART4_RX_PIN            PA1
-#define UART4_TX_PIN            PA0
+#define UART4_TX_PIN            PA0 */
 
 #define USE_UART5
 #define UART5_RX_PIN            PD2
 #define UART5_TX_PIN            PC12
 
+/*
 #define USE_UART6
 #define UART6_RX_PIN            PC7
-#define UART6_TX_PIN            PC6
+#define UART6_TX_PIN            PC6 */
 
 #define SERIAL_PORT_COUNT       7 //VCP, USART1, USART2,USART3,USART4,USART5,USART6
 
