9bd8d4e0a406fd57cebca058da7a9fbb011e4541
adrianmiriuta <adrian.miriuta@gmail.com>
2019-03-12 08:29:31 +0100

    cleanup

--- src/main/target/CLRACINGF7/target.h
+++ src/main/target/CLRACINGF7/target.h
@@ -118,4 +118,4 @@
 #define TARGET_IO_PORTD         (BIT(2))
 
 #define USABLE_TIMER_CHANNEL_COUNT      6
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(4)  )
+#define USED_TIMERS             (TIM_N(2) | TIM_N(3) | TIM_N(4))
