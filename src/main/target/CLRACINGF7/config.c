a51e46bf6e0d1187e30aafe6374a089e40c8c378
adrianmiriuta <adrian.miriuta@gmail.com>
2019-03-15 06:44:01 +0100

    clracing -DYNAMIC_FILTER

--- src/main/target/CLRACINGF7/config.c
+++ src/main/target/CLRACINGF7/config.c
@@ -36,7 +36,7 @@ void targetConfiguration(void) {
     rcControlsConfigMutable()->deadband = 7;
     rcControlsConfigMutable()->yaw_deadband = 11;
 
-    featureSet((FEATURE_AIRMODE | FEATURE_TELEMETRY) ^ DYNAMIC_FILTER);
+    featureSet((FEATURE_AIRMODE | FEATURE_TELEMETRY) ^ FEATURE_DYNAMIC_FILTER);
 
     for (uint8_t pidProfileIndex = 0; pidProfileIndex < MAX_PROFILE_COUNT; pidProfileIndex++) {
         pidProfile_t *pidProfile = pidProfilesMutable(pidProfileIndex);
