6d4ff7a2bd3f5023f73ef2152b9dc85ad9212401
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-05-13 18:01:41 +0200

    cleanup

--- src/main/common/crc.c
+++ src/main/common/crc.c
@@ -1,23 +1,3 @@
-/*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <stdint.h>
 
 #include "streambuf.h"
@@ -111,4 +91,3 @@ void crc8_xor_sbuf_append(sbuf_t *dst, uint8_t *start)
     }
     sbufWriteU8(dst, crc);
 }
-
