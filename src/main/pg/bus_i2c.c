6e3e21ec4c123a82619d3b70ec35f55e18778e7f
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-04 18:13:18 +0200

    cleanup bus_i2c.c

--- src/main/pg/bus_i2c.c
+++ src/main/pg/bus_i2c.c
@@ -5,52 +5,47 @@
 #include "platform.h"
 
 #if defined(USE_I2C) && !defined(SOFT_I2C)
+  #include "common/utils.h"
+  #include "drivers/io.h"
+  #include "pg/pg.h"
+  #include "pg/pg_ids.h"
+  #include "pg/bus_i2c.h"
 
-#include "common/utils.h"
-
-#include "drivers/io.h"
-
-#include "pg/pg.h"
-#include "pg/pg_ids.h"
-
-#include "pg/bus_i2c.h"
-
-typedef struct i2cDefaultConfig_s {
+  typedef struct i2cDefaultConfig_s {
     I2CDevice device;
     ioTag_t ioTagScl, ioTagSda;
     bool overClock;
     bool pullUp;
-} i2cDefaultConfig_t;
+  } i2cDefaultConfig_t;
 
-static const i2cDefaultConfig_t i2cDefaultConfig[] = {
-#ifdef USE_I2C_DEVICE_1
-    { I2CDEV_1, IO_TAG(I2C1_SCL), IO_TAG(I2C1_SDA), I2C1_OVERCLOCK, I2C1_PULLUP },
-#endif
-#ifdef USE_I2C_DEVICE_2
-    { I2CDEV_2, IO_TAG(I2C2_SCL), IO_TAG(I2C2_SDA), I2C2_OVERCLOCK, I2C2_PULLUP },
-#endif
-#ifdef USE_I2C_DEVICE_3
-    { I2CDEV_3, IO_TAG(I2C3_SCL), IO_TAG(I2C3_SDA), I2C3_OVERCLOCK, I2C3_PULLUP },
-#endif
-#ifdef USE_I2C_DEVICE_4
-    { I2CDEV_4, IO_TAG(I2C4_SCL), IO_TAG(I2C4_SDA), I2C4_OVERCLOCK, I2C4_PULLUP },
-#endif
-};
+  static const i2cDefaultConfig_t i2cDefaultConfig[] = {
+    #ifdef USE_I2C_DEVICE_1
+      { I2CDEV_1, IO_TAG(I2C1_SCL), IO_TAG(I2C1_SDA), I2C1_OVERCLOCK, I2C1_PULLUP },
+    #endif
+    #ifdef USE_I2C_DEVICE_2
+      { I2CDEV_2, IO_TAG(I2C2_SCL), IO_TAG(I2C2_SDA), I2C2_OVERCLOCK, I2C2_PULLUP },
+    #endif
+    #ifdef USE_I2C_DEVICE_3
+      { I2CDEV_3, IO_TAG(I2C3_SCL), IO_TAG(I2C3_SDA), I2C3_OVERCLOCK, I2C3_PULLUP },
+    #endif
+    #ifdef USE_I2C_DEVICE_4
+      { I2CDEV_4, IO_TAG(I2C4_SCL), IO_TAG(I2C4_SDA), I2C4_OVERCLOCK, I2C4_PULLUP },
+    #endif
+  };
 
-void pgResetFn_i2cConfig(i2cConfig_t *i2cConfig)
-{
+  void pgResetFn_i2cConfig(i2cConfig_t *i2cConfig) {
     memset(i2cConfig, 0, sizeof(*i2cConfig));
 
     for (size_t index = 0 ; index < ARRAYLEN(i2cDefaultConfig) ; index++) {
-        const i2cDefaultConfig_t *defconf = &i2cDefaultConfig[index];
-        int device = defconf->device;
-        i2cConfig[device].ioTagScl = defconf->ioTagScl;
-        i2cConfig[device].ioTagSda = defconf->ioTagSda;
-        i2cConfig[device].overClock = defconf->overClock;
-        i2cConfig[device].pullUp = defconf->pullUp;
+      const i2cDefaultConfig_t *defconf = &i2cDefaultConfig[index];
+      int device = defconf->device;
+      
+      i2cConfig[device].ioTagScl = defconf->ioTagScl;
+      i2cConfig[device].ioTagSda = defconf->ioTagSda;
+      i2cConfig[device].overClock = defconf->overClock;
+      i2cConfig[device].pullUp = defconf->pullUp;
     }
-}
+  }
 
-PG_REGISTER_ARRAY_WITH_RESET_FN(i2cConfig_t, I2CDEV_COUNT, i2cConfig, PG_I2C_CONFIG, 0);
-
-#endif // defined(USE_I2C) && !defined(USE_SOFT_I2C)
+  PG_REGISTER_ARRAY_WITH_RESET_FN(i2cConfig_t, I2CDEV_COUNT, i2cConfig, PG_I2C_CONFIG, 0);
+#endif //defined(USE_I2C) && !defined(USE_SOFT_I2C)
