5ee3beb745f495811a8fde5a50bd90a4192fc1a3
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-05 16:33:26 +0200

    cleanup filter.c filter.h

--- src/main/common/filter.c
+++ src/main/common/filter.c
@@ -17,210 +17,200 @@
 
 // NULL filter
 FAST_CODE float nullFilterApply(filter_t *filter, float input) {
-    UNUSED(filter);
-    return (input);
+  UNUSED(filter);
+  return (input);
 }
-
 
 // PT1 Low Pass filter
 float pt1FilterGain(uint16_t f_cut, float dT) {
-    float RC = 1 / ( 2 * M_PI_FLOAT * f_cut);
-    return (dT / (RC + dT));
+  float RC = 1 / ( 2 * M_PI_FLOAT * f_cut);
+  return (dT / (RC + dT));
 }
 
 void pt1FilterInit(pt1Filter_t *filter, float k) {
-    filter->state = 0.0f;
-    filter->k = k;
+  filter->state = 0.0f;
+  filter->k = k;
 }
 
 void pt1FilterUpdateCutoff(pt1Filter_t *filter, float k) {
-    filter->k = k;
+  filter->k = k;
 }
 
 FAST_CODE float pt1FilterApply(pt1Filter_t *filter, float input) {
-    filter->state = filter->state + filter->k * (input - filter->state);
-    return (filter->state);
+  filter->state = filter->state + filter->k * (input - filter->state);
+  return (filter->state);
 }
 
 // Slew filter with limit
 void slewFilterInit(slewFilter_t *filter, float slewLimit, float threshold) {
-    filter->state = 0.0f;
-    filter->slewLimit = slewLimit;
-    filter->threshold = threshold;
+  filter->state = 0.0f;
+  filter->slewLimit = slewLimit;
+  filter->threshold = threshold;
 }
 
 FAST_CODE float slewFilterApply(slewFilter_t *filter, float input) {
-    if (filter->state >= filter->threshold) {
-        if (input >= filter->state - filter->slewLimit) {
-            filter->state = input;
-        }
-    } else if (filter->state <= -filter->threshold) {
-        if (input <= filter->state + filter->slewLimit) {
-            filter->state = input;
-        }
-    } else {
-        filter->state = input;
+  if (filter->state >= filter->threshold) {
+    if (input >= filter->state - filter->slewLimit) {
+      filter->state = input;
     }
-    return (filter->state);
+  } else if (filter->state <= -filter->threshold) {
+    if (input <= filter->state + filter->slewLimit) {
+      filter->state = input;
+    }
+  } else {
+    filter->state = input;
+  }
+  return (filter->state);
 }
 
 // get notch filter Q given center frequency (f0) and lower cutoff frequency (f1)
 // Q = f0 / (f2 - f1) ; f2 = f0^2 / f1
 float filterGetNotchQ(float centerFreq, float cutoffFreq) {
-    return (centerFreq * cutoffFreq / (centerFreq * centerFreq - cutoffFreq * cutoffFreq));
+  return (centerFreq * cutoffFreq / (centerFreq * centerFreq - cutoffFreq * cutoffFreq));
 }
 
 // biquad Filter
 void biquadFilterInitLPF(biquadFilter_t *filter, float filterFreq, uint32_t refreshRate) {
-    biquadFilterInit(filter, filterFreq, refreshRate, BIQUAD_Q, FILTER_LPF);
+  biquadFilterInit(filter, filterFreq, refreshRate, BIQUAD_Q, FILTER_LPF);
 }
 
 void biquadFilterInit(biquadFilter_t *filter, float filterFreq, uint32_t refreshRate, float Q, biquadFilterType_e filterType) {
-    // setup variables
-    const float omega = 2.0f * M_PI_FLOAT * filterFreq * refreshRate * 0.000001f;
-    const float sn = sin_approx(omega);
-    const float cs = cos_approx(omega);
-    const float alpha = sn / (2.0f * Q);
+  // setup variables
+  const float omega = 2.0f * M_PI_FLOAT * filterFreq * refreshRate * 0.000001f;
+  const float sn = sin_approx(omega);
+  const float cs = cos_approx(omega);
+  const float alpha = sn / (2.0f * Q);
+  float b0 = 0, b1 = 0, b2 = 0, a0 = 0, a1 = 0, a2 = 0;
 
-    float b0 = 0, b1 = 0, b2 = 0, a0 = 0, a1 = 0, a2 = 0;
-
-    switch (filterType) {
+  switch (filterType) {
     case FILTER_LPF:
-        // 2nd order Butterworth (with Q=1/sqrt(2)) / Butterworth biquad section with Q
-        // described in http://www.ti.com/lit/an/slaa447/slaa447.pdf
-        b0 = (1 - cs) * 0.5f;
-        b1 = 1 - cs;
-        b2 = (1 - cs) * 0.5f;
-        a0 = 1 + alpha;
-        a1 = -2 * cs;
-        a2 = 1 - alpha;
-        break;
+      // 2nd order Butterworth (with Q=1/sqrt(2)) / Butterworth biquad section with Q
+      // described in http://www.ti.com/lit/an/slaa447/slaa447.pdf
+      b0 = (1 - cs) * 0.5f;
+      b1 = 1 - cs;
+      b2 = (1 - cs) * 0.5f;
+      a0 = 1 + alpha;
+      a1 = -2 * cs;
+      a2 = 1 - alpha;
+      break;
     case FILTER_NOTCH:
-        b0 =  1;
-        b1 = -2 * cs;
-        b2 =  1;
-        a0 =  1 + alpha;
-        a1 = -2 * cs;
-        a2 =  1 - alpha;
-        break;
+      b0 =  1;
+      b1 = -2 * cs;
+      b2 =  1;
+      a0 =  1 + alpha;
+      a1 = -2 * cs;
+      a2 =  1 - alpha;
+      break;
     case FILTER_BPF:
-        b0 = alpha;
-        b1 = 0;
-        b2 = -alpha;
-        a0 = 1 + alpha;
-        a1 = -2 * cs;
-        a2 = 1 - alpha;
-        break;
-    }
+      b0 = alpha;
+      b1 = 0;
+      b2 = -alpha;
+      a0 = 1 + alpha;
+      a1 = -2 * cs;
+      a2 = 1 - alpha;
+      break;
+  }
 
-    // precompute the coefficients
-    filter->b0 = b0 / a0;
-    filter->b1 = b1 / a0;
-    filter->b2 = b2 / a0;
-    filter->a1 = a1 / a0;
-    filter->a2 = a2 / a0;
+  // precompute the coefficients
+  filter->b0 = b0 / a0;
+  filter->b1 = b1 / a0;
+  filter->b2 = b2 / a0;
+  filter->a1 = a1 / a0;
+  filter->a2 = a2 / a0;
 
-    // zero initial samples
-    filter->x1 = filter->x2 = 0;
-    filter->y1 = filter->y2 = 0;
+  // zero initial samples
+  filter->x1 = filter->x2 = 0;
+  filter->y1 = filter->y2 = 0;
 }
 
 FAST_CODE void biquadFilterUpdate(biquadFilter_t *filter, float filterFreq, uint32_t refreshRate, float Q, biquadFilterType_e filterType) {
-    // backup state
-    float x1 = filter->x1;
-    float x2 = filter->x2;
-    float y1 = filter->y1;
-    float y2 = filter->y2;
+  // backup state
+  float x1 = filter->x1;
+  float x2 = filter->x2;
+  float y1 = filter->y1;
+  float y2 = filter->y2;
 
-    biquadFilterInit(filter, filterFreq, refreshRate, Q, filterType);
+  biquadFilterInit(filter, filterFreq, refreshRate, Q, filterType);
 
-    // restore state
-    filter->x1 = x1;
-    filter->x2 = x2;
-    filter->y1 = y1;
-    filter->y2 = y2;
+  // restore state
+  filter->x1 = x1;
+  filter->x2 = x2;
+  filter->y1 = y1;
+  filter->y2 = y2;
 }
 
 FAST_CODE void biquadFilterUpdateLPF(biquadFilter_t *filter, float filterFreq, uint32_t refreshRate) {
-    biquadFilterUpdate(filter, filterFreq, refreshRate, BIQUAD_Q, FILTER_LPF);
+  biquadFilterUpdate(filter, filterFreq, refreshRate, BIQUAD_Q, FILTER_LPF);
 }
 
-/* Computes a biquadFilter_t filter on a sample (slightly less precise than df2 but works in dynamic mode) */
+// Computes a biquadFilter_t filter on a sample (slightly less precise than df2 but works in dynamic mode)
 FAST_CODE float biquadFilterApplyDF1(biquadFilter_t *filter, float input) {
-    /* compute result */
-    const float result = filter->b0 * input + filter->b1 * filter->x1 + filter->b2 * filter->x2 - filter->a1 * filter->y1 - filter->a2 * filter->y2;
+  // compute result
+  const float result = filter->b0 * input + filter->b1 * filter->x1 + filter->b2 * filter->x2 - filter->a1 * filter->y1 - filter->a2 * filter->y2;
 
-    /* shift x1 to x2, input to x1 */
-    filter->x2 = filter->x1;
-    filter->x1 = input;
+  // shift x1 to x2, input to x1
+  filter->x2 = filter->x1;
+  filter->x1 = input;
 
-    /* shift y1 to y2, result to y1 */
-    filter->y2 = filter->y1;
-    filter->y1 = result;
+  // shift y1 to y2, result to y1
+  filter->y2 = filter->y1;
+  filter->y1 = result;
 
-    return (result);
+  return (result);
 }
 
 /* Computes a biquadFilter_t filter in direct form 2 on a sample (higher precision but can't handle changes in coefficients */
 FAST_CODE float biquadFilterApply(biquadFilter_t *filter, float input) {
-    const float result = filter->b0 * input + filter->x1;
-    filter->x1 = filter->b1 * input - filter->a1 * result + filter->x2;
-    filter->x2 = filter->b2 * input - filter->a2 * result;
-    return (result);
+  const float result = filter->b0 * input + filter->x1;
+  filter->x1 = filter->b1 * input - filter->a1 * result + filter->x2;
+  filter->x2 = filter->b2 * input - filter->a2 * result;
+  return (result);
 }
 
 // kalman filter
 void kalmanInit(kalman_t *filter, float q, uint32_t w) {
-    memset(filter, 0, sizeof(kalman_t));
-    filter->q     = q * 0.001f;
-    filter->w     = w;
+  memset(filter, 0, sizeof(kalman_t));
+  filter->q = q * 0.001f;
+  filter->w = w;
 }
 
 #pragma GCC push_options
 #pragma GCC optimize("O3")
 
-//#define VARIANCE_SCALE 0.333f
 #define VARIANCE_SCALE 1.0f
 FAST_CODE float kalmanUpdate(kalman_t *filter, float input) {
-    const float windowSizeInverse = 1.0f/(filter->w - 1);
+  const float windowSizeInverse = 1.0f/(filter->w - 1);
 
-    // project the state ahead using acceleration
-    filter->x += (filter->x - filter->lastX);
+  // project the state ahead using acceleration
+  filter->x += (filter->x - filter->lastX);
+  // update last state
+  filter->lastX = filter->x;
+  // prediction update
+  filter->p = filter->p + filter->q;
+  // measurement update
+  filter->k = filter->p / (filter->p + filter->r);
+  filter->x += filter->k * (input - filter->x);
+  filter->p = (1.0f - filter->k) * filter->p;
 
-    // update last state
-    filter->lastX = filter->x;
+  DEBUG_SET(DEBUG_KALMAN, 0, lrintf(filter->q * 1000));
+  DEBUG_SET(DEBUG_KALMAN, 1, lrintf(filter->r * 1000));
+  DEBUG_SET(DEBUG_KALMAN, 2, lrintf(filter->p * 1000));
+  DEBUG_SET(DEBUG_KALMAN, 3, lrintf(filter->k * 1000));
 
-    // prediction update
-    filter->p = filter->p + filter->q;
+  // update variance
+  filter->window[filter->windowIndex] = input;
+  filter->meanSum += filter->window[filter->windowIndex];
+  filter->varianceSum = filter->varianceSum + (filter->window[filter->windowIndex] * filter->window[filter->windowIndex]);
+  if (++filter->windowIndex >= filter->w) {
+      filter->windowIndex = 0;
+  }
+  filter->meanSum -= filter->window[filter->windowIndex];
+  filter->varianceSum = filter->varianceSum - (filter->window[filter->windowIndex] * filter->window[filter->windowIndex]);
+  filter->mean = filter->meanSum * windowSizeInverse;
+  filter->variance = ABS(filter->varianceSum * windowSizeInverse - (filter->mean * filter->mean));
+  filter->r = sqrtf(filter->variance) * VARIANCE_SCALE;
 
-    // measurement update
-    filter->k = filter->p / (filter->p + filter->r);
-    filter->x += filter->k * (input - filter->x);
-    filter->p = (1.0f - filter->k) * filter->p;
-
-    DEBUG_SET(DEBUG_KALMAN, 0, lrintf(filter->q * 1000));
-    DEBUG_SET(DEBUG_KALMAN, 1, lrintf(filter->r * 1000));
-    DEBUG_SET(DEBUG_KALMAN, 2, lrintf(filter->p * 1000));
-    DEBUG_SET(DEBUG_KALMAN, 3, lrintf(filter->k * 1000));
-
-    // update variance
-    filter->window[filter->windowIndex] = input;
-
-    filter->meanSum += filter->window[filter->windowIndex];
-    filter->varianceSum = filter->varianceSum + (filter->window[filter->windowIndex] * filter->window[filter->windowIndex]);
-
-    if (++filter->windowIndex >= filter->w) {
-        filter->windowIndex = 0;
-    }
-
-    filter->meanSum -= filter->window[filter->windowIndex];
-    filter->varianceSum = filter->varianceSum - (filter->window[filter->windowIndex] * filter->window[filter->windowIndex]);
-
-    filter->mean = filter->meanSum * windowSizeInverse;
-    filter->variance = ABS(filter->varianceSum * windowSizeInverse - (filter->mean * filter->mean));
-    filter->r = sqrtf(filter->variance) * VARIANCE_SCALE;
-
-    return (filter->x);
+  return (filter->x);
 }
 
 #pragma GCC pop_options
--- src/main/common/filter.h
+++ src/main/common/filter.h
@@ -5,55 +5,55 @@ struct filter_s;
 typedef struct filter_s filter_t;
 
 typedef struct pt1Filter_s {
-    float state;
-    float k;
+  float state;
+  float k;
 } pt1Filter_t;
 
 typedef struct slewFilter_s {
-    float state;
-    float slewLimit;
-    float threshold;
+  float state;
+  float slewLimit;
+  float threshold;
 } slewFilter_t;
 
 /* this holds the data required to update samples thru a filter */
 typedef struct biquadFilter_s {
-    float b0, b1, b2, a1, a2;
-    float x1, x2, y1, y2;
+  float b0, b1, b2, a1, a2;
+  float x1, x2, y1, y2;
 } biquadFilter_t;
 
 typedef enum {
-    FILTER_PT1 = 0,
-    FILTER_BIQUAD,
-    FILTER_KALMAN,
+  FILTER_PT1 = 0,
+  FILTER_BIQUAD,
+  FILTER_KALMAN,
 } lowpassFilterType_e;
 
 typedef enum {
-    FILTER_LPF,    // 2nd order Butterworth section
-    FILTER_NOTCH,
-    FILTER_BPF,
+  FILTER_LPF,    // 2nd order Butterworth section
+  FILTER_NOTCH,
+  FILTER_BPF,
 } biquadFilterType_e;
 
 #if (defined(STM32F7) || defined(STM32F4))
-#define MAX_WINDOW_SIZE 256
+  #define MAX_WINDOW_SIZE 256
 #else
-#define MAX_WINDOW_SIZE 64
+  #define MAX_WINDOW_SIZE 64
 #endif
 
 typedef struct kalman_s {
-    uint32_t w;    // window size
-    float q;       // process noise covariance
-    float r;       // measurement noise covariance
-    float p;       // estimation error covariance matrix
-    float k;       // kalman gain
-    float x;       // state
-    float lastX;   // previous state
+  uint32_t w;    // window size
+  float q;       // process noise covariance
+  float r;       // measurement noise covariance
+  float p;       // estimation error covariance matrix
+  float k;       // kalman gain
+  float x;       // state
+  float lastX;   // previous state
 
-    float window[MAX_WINDOW_SIZE];
-    float variance;
-    float varianceSum;
-    float mean;
-    float meanSum;
-    uint32_t windowIndex;
+  float window[MAX_WINDOW_SIZE];
+  float variance;
+  float varianceSum;
+  float mean;
+  float meanSum;
+  uint32_t windowIndex;
 } kalman_t;
 
 typedef float (*filterApplyFnPtr)(filter_t *filter, float input);
