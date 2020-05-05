8c7fd246aa693faccef3a7f9c4cac4757624a9cf
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-05 20:13:04 +0200

    cleanup huffman.h

--- src/main/common/huffman.h
+++ src/main/common/huffman.h
@@ -4,21 +4,21 @@
 
 #define HUFFMAN_TABLE_SIZE 257 // 256 characters plus EOF
 typedef struct huffmanTable_s {
-    uint8_t     codeLen;
-    uint16_t    code;
+  uint8_t     codeLen;
+  uint16_t    code;
 } huffmanTable_t;
 
 typedef struct huffmanState_s {
-    uint16_t    bytesWritten;
-    uint8_t     *outByte;
-    uint16_t    outBufLen;
-    uint8_t     outBit;
+  uint16_t    bytesWritten;
+  uint8_t     *outByte;
+  uint16_t    outBufLen;
+  uint8_t     outBit;
 } huffmanState_t;
 
 extern const huffmanTable_t huffmanTable[HUFFMAN_TABLE_SIZE];
 
 struct huffmanInfo_s {
-    uint16_t uncompressedByteCount;
+  uint16_t uncompressedByteCount;
 };
 
 #define HUFFMAN_INFO_SIZE sizeof(struct huffmanInfo_s)
