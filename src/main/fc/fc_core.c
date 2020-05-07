1a28293bed11b0965592cca710435ea19400be15
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-07 08:42:58 +0200

    corr motor index

--- src/main/fc/fc_core.c
+++ src/main/fc/fc_core.c
@@ -908,7 +908,7 @@ FAST_CODE void taskMainPidLoop(timeUs_t currentTimeUs) {
       subTaskRcCommand(currentTimeUs);
     }
     subTaskPidController(currentTimeUs);
-    if(motorOutputIndex % 3) {
+    if((motorOutputIndex % 3) == 0) {
       subTaskMotorUpdate(currentTimeUs);
     }
     subTaskPidSubprocesses(currentTimeUs);
