fdf544b2b55ffe90cd7276e0a772fa5f3215febd
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-06-04 19:26:14 +0200

    cleanup adcinternal.c

--- src/main/sensors/adcinternal.c
+++ src/main/sensors/adcinternal.c
@@ -5,92 +5,84 @@
 
 #if defined(USE_ADC) && defined(USE_ADC_INTERNAL)
 
-#include "build/debug.h"
+  #include "build/debug.h"
+  #include "common/utils.h"
+  #include "drivers/adc.h"
 
-#include "common/utils.h"
-
-#include "drivers/adc.h"
-
-typedef struct movingAverageStateUint16_s {
+  typedef struct movingAverageStateUint16_s {
     uint32_t sum;
     uint16_t *values;
     uint8_t size;
     uint8_t pos;
-} movingAverageStateUint16_t;
+  } movingAverageStateUint16_t;
 
-uint16_t updateMovingAverageUint16(movingAverageStateUint16_t *state, uint16_t newValue)
-{
+  uint16_t updateMovingAverageUint16(movingAverageStateUint16_t *state, uint16_t newValue) {
     state->sum -= state->values[state->pos];
     state->values[state->pos] = newValue;
     state->sum += newValue;
     state->pos = (state->pos + 1) % state->size;
 
-    return state->sum / state->size;
-}
+    return (state->sum / state->size);
+  }
 
-static uint16_t adcVrefintValue;
-static uint16_t adcVrefintValues[8];
-movingAverageStateUint16_t adcVrefintAverageState = { 0, adcVrefintValues, 8, 0 } ;
+  static uint16_t adcVrefintValue;
+  static uint16_t adcVrefintValues[8];
+  movingAverageStateUint16_t adcVrefintAverageState = { 0, adcVrefintValues, 8, 0 } ;
 
-static uint16_t adcTempsensorValue;
-static uint16_t adcTempsensorValues[8];
-movingAverageStateUint16_t adcTempsensorAverageState = { 0, adcTempsensorValues, 8, 0 } ;
+  static uint16_t adcTempsensorValue;
+  static uint16_t adcTempsensorValues[8];
+  movingAverageStateUint16_t adcTempsensorAverageState = { 0, adcTempsensorValues, 8, 0 } ;
 
-static int16_t coreTemperature;
+  static int16_t coreTemperature;
 
-uint16_t getVrefMv(void)
-{
-#ifdef ADC_VOLTAGE_REFERENCE_MV
-    return ADC_VOLTAGE_REFERENCE_MV;
-#else
-    return 3300 * adcVrefintValue / adcVREFINTCAL;
-#endif
-}
+  uint16_t getVrefMv(void) {
+    #ifdef ADC_VOLTAGE_REFERENCE_MV
+      return ADC_VOLTAGE_REFERENCE_MV;
+    #else
+      return (3300 * adcVrefintValue / adcVREFINTCAL);
+    #endif
+  }
 
-int16_t getCoreTemperatureCelsius(void)
-{
+  int16_t getCoreTemperatureCelsius(void) {
     return coreTemperature;
-}
+  }
 
-void adcInternalProcess(timeUs_t currentTimeUs)
-{
-    UNUSED(currentTimeUs);
+  void adcInternalProcess(timeUs_t currentTimeUs) {
+      UNUSED(currentTimeUs);
 
-    if (adcInternalIsBusy()) {
+      if (adcInternalIsBusy()) {
         return;
-    }
+      }
 
-    uint16_t vrefintSample = adcInternalReadVrefint();
-    uint16_t tempsensorSample = adcInternalReadTempsensor();
+      uint16_t vrefintSample = adcInternalReadVrefint();
+      uint16_t tempsensorSample = adcInternalReadTempsensor();
 
-    adcVrefintValue = updateMovingAverageUint16(&adcVrefintAverageState, vrefintSample);
-    adcTempsensorValue = updateMovingAverageUint16(&adcTempsensorAverageState, tempsensorSample);
+      adcVrefintValue = updateMovingAverageUint16(&adcVrefintAverageState, vrefintSample);
+      adcTempsensorValue = updateMovingAverageUint16(&adcTempsensorAverageState, tempsensorSample);
 
-    int32_t adcTempsensorAdjusted = (int32_t)adcTempsensorValue * 3300 / getVrefMv();
-    coreTemperature = ((adcTempsensorAdjusted - adcTSCAL1) * adcTSSlopeK + 30 * 1000 + 500) / 1000;
+      int32_t adcTempsensorAdjusted = (int32_t)adcTempsensorValue * 3300 / getVrefMv();
+      coreTemperature = ((adcTempsensorAdjusted - adcTSCAL1) * adcTSSlopeK + 30 * 1000 + 500) / 1000;
 
-    DEBUG_SET(DEBUG_CORE_TEMP, 0, coreTemperature);
+      DEBUG_SET(DEBUG_CORE_TEMP, 0, coreTemperature);
 
-    adcInternalStartConversion(); // Start next conversion
-}
+      adcInternalStartConversion(); // Start next conversion
+  }
 
-void adcInternalInit(void)
-{
+  void adcInternalInit(void) {
     // Call adcInternalProcess repeatedly to fill moving average array
     for (int i = 0 ; i < 9 ; i++) {
-        while (adcInternalIsBusy()) {
-            // empty
-        }
-        adcInternalProcess(0);
+      while (adcInternalIsBusy()) {
+        // empty
+      }
+      adcInternalProcess(0);
     }
-}
+  }
 #else
-uint16_t getVrefMv(void)
-{
-#ifdef ADC_VOLTAGE_REFERENCE_MV
+  uint16_t getVrefMv(void) {
+  #ifdef ADC_VOLTAGE_REFERENCE_MV
     return ADC_VOLTAGE_REFERENCE_MV;
-#else
+  #else
     return 3300;
-#endif
-}
+  #endif
+  }
 #endif
