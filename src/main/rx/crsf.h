abfb8a0c0811e0e86ce8cb01c3a0c6ddbe69ae01
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-06-15 14:34:44 +0200

    cleanup crsf.h

--- src/main/rx/crsf.h
+++ src/main/rx/crsf.h
@@ -9,15 +9,15 @@
 #define CRSF_MAX_CHANNEL        16
 
 typedef struct crsfFrameDef_s {
-    uint8_t deviceAddress;
-    uint8_t frameLength;
-    uint8_t type;
-    uint8_t payload[CRSF_PAYLOAD_SIZE_MAX + 1]; // +1 for CRC at end of payload
+  uint8_t deviceAddress;
+  uint8_t frameLength;
+  uint8_t type;
+  uint8_t payload[CRSF_PAYLOAD_SIZE_MAX + 1]; // +1 for CRC at end of payload
 } crsfFrameDef_t;
 
 typedef union crsfFrame_u {
-    uint8_t bytes[CRSF_FRAME_SIZE_MAX];
-    crsfFrameDef_t frame;
+  uint8_t bytes[CRSF_FRAME_SIZE_MAX];
+  crsfFrameDef_t frame;
 } crsfFrame_t;
 
 void crsfRxWriteTelemetryData(const void *data, int len);
