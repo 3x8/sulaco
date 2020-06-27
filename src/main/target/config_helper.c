7c416beea1eb556aeb29170f532398db4270ace8
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-06-27 21:05:27 +0200

    cleanup config

--- src/main/target/config_helper.c
+++ src/main/target/config_helper.c
@@ -1,14 +1,12 @@
 #include "config_helper.h"
 
 #ifdef USE_TARGET_CONFIG
-
-void targetSerialPortFunctionConfig(targetSerialPortFunction_t *config, size_t count)
-{
+  void targetSerialPortFunctionConfig(targetSerialPortFunction_t *config, size_t count) {
     for (size_t i = 0 ; i < count ; i++) {
-        int index = findSerialPortIndexByIdentifier(config[i].identifier);
-        if (index >= 0) {
-            serialConfigMutable()->portConfigs[index].functionMask = config[i].function;
-        }
+      int index = findSerialPortIndexByIdentifier(config[i].identifier);
+      if (index >= 0) {
+        serialConfigMutable()->portConfigs[index].functionMask = config[i].function;
+      }
     }
-}
+  }
 #endif
--- src/main/target/config_helper.h
+++ src/main/target/config_helper.h
@@ -3,8 +3,8 @@
 #include "io/serial.h"
 
 typedef struct targetSerialPortFunction_s {
-    serialPortIdentifier_e identifier;
-    serialPortFunction_e   function;
+  serialPortIdentifier_e identifier;
+  serialPortFunction_e   function;
 } targetSerialPortFunction_t;
 
 void targetSerialPortFunctionConfig(targetSerialPortFunction_t *config, size_t count);
