dacf932ab10c2e5da028d2849f0e78cba215155a
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-08-11 13:28:13 +0200

    cleanup

--- src/main/target/HELIOMOD/target.mk
+++ src/main/target/HELIOMOD/target.mk
@@ -7,4 +7,4 @@ TARGET_SRC =  \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-             drivers/max7456.c
+            drivers/max7456.c
