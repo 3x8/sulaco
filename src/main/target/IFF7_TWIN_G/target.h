b8bb5c5f45a8964cda48c80d99f8675ec84fff16
Adrian Miriuta <a.miriuta@tubesolar.de>
2020-08-24 10:44:18 +0200

    bearbeiten

--- src/main/target/IFF7_TWIN_G/target.h
+++ src/main/target/IFF7_TWIN_G/target.h
@@ -1,36 +1,28 @@
-/*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #define TARGET_BOARD_IDENTIFIER "S7X2"
+#define USBD_PRODUCT_STRING     "IFLIGHTF7TWING"
+#define TARGET_MANUFACTURER_IDENTIFIER  "IFL"
 
-#define USBD_PRODUCT_STRING     "IFLIGHT F7 TWIN G"
+#undef USE_BOARD_INFO
 
-#define USE_DUAL_GYRO
+#define USE_TARGET_CONFIG
 
+#define USE_DSHOT_DMAR
 #define ENABLE_DSHOT_DMAR       true
+
+#define LED0_PIN                PC4
+#define USE_BEEPER
+#define BEEPER_PIN              PC15
+#define BEEPER_INVERTED
 
 #define CAMERA_CONTROL_PIN      PA0
 
-#define LED0_PIN                PC4
+#define USE_BUTTERED_PIDS       true
+
+#define DEFAULT_PIDS_ROLL   {27, 17, 37, 0}
+#define DEFAULT_PIDS_PITCH  {27, 17, 37, 0}
+#define DEFAULT_PIDS_YAW    {41, 51, 1, 0}
 
 #define USE_PINIO
 #define USE_PINIOBOX
@@ -38,9 +30,7 @@
 #define PINIO2_PIN              PC14
 #define PINIO3_PIN              PB8
 
-#define USE_BEEPER
-#define BEEPER_PIN              PC15
-#define BEEPER_INVERTED
+#define USE_DUAL_GYRO
 
 #define USE_EXTI
 #define MPU_INT_EXTI
@@ -71,29 +61,44 @@
 
 #define GYRO_CONFIG_USE_GYRO_DEFAULT GYRO_CONFIG_USE_GYRO_BOTH
 
-#define USE_VCP
-#define USE_UART1
-#define USE_UART2
-#define USE_UART3
-#define USE_UART4
-#define USE_UART5
-#define SERIAL_PORT_COUNT       6
+// OSD
+#define USE_MAX7456
+#define MAX7456_SPI_INSTANCE    SPI2
+#define MAX7456_SPI_CS_PIN      SPI2_NSS_PIN
 
+// FLASH
+#define FLASH_SPI_INSTANCE      SPI3
+#define FLASH_CS_PIN            SPI3_NSS_PIN
+
+#define USE_FLASH
+#define USE_FLASH_W25N01G
+//#define USE_FLASH_M25P16
+#define USE_FLASHFS
+
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+
+// UARTS
+#define USE_VCP
+
+#define USE_UART1
 #define UART1_TX_PIN            PA9
 #define UART1_RX_PIN            PA10
-
+#define USE_UART2
 #define UART2_TX_PIN            PA2
 #define UART2_RX_PIN            PA3
-
+#define USE_UART3
 #define UART3_TX_PIN            PB10
 #define UART3_RX_PIN            PB11
-
+#define USE_UART4
 #define UART4_TX_PIN            PC10
 #define UART4_RX_PIN            PC11
-
+#define USE_UART5
 #define UART5_TX_PIN            PC12
 #define UART5_RX_PIN            PD2
 
+#define SERIAL_PORT_COUNT       6
+
+#define USE_ESCSERIAL
 
 #define USE_SPI
 
@@ -103,50 +108,33 @@
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
 
-#define USE_SPI_DEVICE_2 // MAX7456
+#define USE_SPI_DEVICE_2
 #define SPI2_NSS_PIN            PB12
 #define SPI2_SCK_PIN            PB13
 #define SPI2_MISO_PIN           PB14
 #define SPI2_MOSI_PIN           PB15
 
-#define USE_OSD
-#define USE_MAX7456
-#define MAX7456_SPI_INSTANCE    SPI2
-#define MAX7456_SPI_CS_PIN      PB12
-
-#define USE_SPI_DEVICE_3 // FLASH
+#define USE_SPI_DEVICE_3
 #define SPI3_NSS_PIN            PB9
 #define SPI3_SCK_PIN            PB3
 #define SPI3_MISO_PIN           PB4
 #define SPI3_MOSI_PIN           PB5
 
-#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
-#define USE_FLASHFS
-#define USE_FLASH_M25P16
-#define FLASH_CS_PIN            PB9
-#define FLASH_SPI_INSTANCE      SPI3
-
 #define USE_ADC
 #define ADC_INSTANCE            ADC3
 #define ADC3_DMA_STREAM         DMA2_Stream0
-
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define VBAT_ADC_PIN            PC1
 #define CURRENT_METER_ADC_PIN   PC2
 #define RSSI_ADC_PIN            PC0
-
 #define CURRENT_METER_SCALE_DEFAULT 100
 
-#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
-#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
-
-#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 #define DEFAULT_RX_FEATURE                  FEATURE_RX_SERIAL
-#define DEFAULT_FEATURES                    (FEATURE_TRANSPONDER | FEATURE_RSSI_ADC | FEATURE_TELEMETRY | FEATURE_OSD | FEATURE_LED_STRIP)
-
 #define SERIALRX_UART                       SERIAL_PORT_USART2
-#define SERIALRX_PROVIDER                   SERIALRX_SBUS
+#define DEFAULT_FEATURES                    (FEATURE_TELEMETRY)
 
-#define SENSORS_SET             (SENSOR_ACC | SENSOR_BARO)
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
 #define TARGET_IO_PORTA         0xffff
 #define TARGET_IO_PORTB         0xffff
